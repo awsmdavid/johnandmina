ac986fce5867e7db06b88714dab9d91f41c5c3ff
awsmdavid <awsmdavid@gmail.com>
2015-03-04 00:23:21 -0500

    updated countdown

--- js/app.js
+++ js/app.js
@@ -3,4 +3,46 @@
 $(document).foundation();
 $(document).ready(function(){
 
+var end = new Date('08/29/2015 10:1 AM');
+
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+
+function showRemaining() {
+    var now = new Date();
+    var distance = end - now;
+    var days = Math.floor(distance / _day);
+    if (days === 0) {
+        try{
+            clearInterval(timer);
+			document.getElementById('countdowndays').innerHTML = "Today is the day!";
+            return;
+        }
+        catch(err){}
+    }
+    if (distance > 0) {
+		try{
+			document.getElementById('countdowndays').innerHTML = "The wedding is in " + days + " days!";
+        }
+        catch(err){}
+    }
+
+    if (distance < 0) {
+        try{
+            clearInterval(timer);
+			document.getElementById('countdowndays').innerHTML = -days + " days married!";
+            return;
+        }
+        catch(err){}
+    }
+
+
+
+
+}
+
+timer = setInterval(showRemaining, 1000);
 });
