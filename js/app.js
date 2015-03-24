81b765b084add99c9d4e5f92fb8ef850df091d24
awsmdavid <awsmdavid@gmail.com>
2015-03-23 21:06:40 -0400

    add bg

--- js/app.js
+++ js/app.js
@@ -46,3 +46,31 @@ function showRemaining() {
 
 timer = setInterval(showRemaining, 1000);
 });
+
+
+function login() {
+
+    // set hashword (what hash of (password + random hash) equals)
+    var theHashword = "2879a3a3b23a9f5b9ead69529bb5f3c8";
+
+    // "enter key" also triggers submit click
+    $(".content-password").keyup(function(event){
+        if(event.keyCode == 13){
+            $('#submit-password-button').click();
+        }
+    });
+
+    // get user submitted password
+    var password = document.getElementById("password").value;
+
+    // if password is correct
+    if (CryptoJS.MD5(password+"2aecc21ce57c973d624175017c3f4616")==theHashword){
+        // open new window
+        window.open('rsvp'+ CryptoJS.MD5(password) +'.html', 'rsvp!', '_blank');
+    }
+    // if incorrect password
+    else {
+        $('#incorrect-password-message').replaceWith("<span class="+"incorrect-pw-msg panel"+">Incorrect password, please try again!</span>");
+        $('form').get(0).reset();
+    }
+}
